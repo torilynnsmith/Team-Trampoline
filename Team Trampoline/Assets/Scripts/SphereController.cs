493593ad7677d75adb941fa15e487d57639839fc
Tori Smith <torilynnsmith@gmail.com>
2022-09-15 12:13:11 -0400

    Drag&Drop Color fixes

--- Team Trampoline/Assets/Scripts/SphereController.cs
+++ Team Trampoline/Assets/Scripts/SphereController.cs
@@ -2,45 +2,51 @@ using UnityEngine;
 
 public class SphereController : MonoBehaviour, IDrag //inherit from our interface, hover and CTRL + .
 {
+    //3D OBJECT VARIABLES
+    [SerializeField]
     private Rigidbody rb; //ref RigidBody on attached object
-        //WHERE I LEFT OFF
-        //NOTE: alter these into an if statement? not everything will have a rigidbody, but I want to code this for both.
 
     private Renderer objectRenderer; //name variable for objectRenderer
+    //private SpriteRenderer spriteRenderer; //name variable for spriteRenderer
     private Color objectColor; //name variable for objectColor
-
-    //private float randomChannelOne, randomChannelTwo, randomChannelThree; //color channels
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
-        objectRenderer = GetComponent<Renderer>(); 
+        if(GetComponent<Rigidbody>())
+        rb = GetComponent<Rigidbody>(); //get RigidBody if there is a RigidBody Component on the Object
+        objectRenderer = GetComponent<Renderer>(); //get Renderer Component (for 3D)
+
+        //if (spriteRenderer) spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void onStartDrag()
     {
         //throw new System.NotImplementedException();
-        rb.useGravity = false; //ignore gravity when dragging
+        if(rb) rb.useGravity = false; //ignore gravity when dragging
 
     }
 
     public void onEndDrag()
     {
         //throw new System.NotImplementedException();
-        rb.useGravity = true; //reinstate gravity when done dragging
-        rb.velocity = Vector3.zero; //object will drop straight down when dropped
-
-        //Randomize color channel values on End Drag
-        //randomChannelOne = Random.Range(0f, 1f);
-        //randomChannelTwo = Random.Range(0f, 1f);
-        //randomChannelThree = Random.Range(0f, 1f);
+        if (rb != null)
+        {
+            rb.useGravity = true; //reinstate gravity when done dragging
+            rb.velocity = Vector3.zero; //object will drop straight down when dropped
+                //WHY IS THIS NOT WORKING ANYMORE? 
+        }
 
         //CHANGE COLOR
             //WHERE I LEFT OFF
             //CHECK IF OBJECT IS IN THE CORRECT PLACE, IF YES THEN OBJECT IS GREEN, IF NOT OBJECT IS RED
-        objectColor = new Color(1f, 0, 0); //change color to red (1f,0,0 = 255,0,0) 
 
-        objectRenderer.material.SetColor("_Color", objectColor); //set new color to sphere
+        objectColor = new Color(255, 0, 0); //change color to red (1f,0,0 = 255,0,0)
+
+        //objectRenderer.material.SetColor("_Color", objectColor); //set new color to sphere (currently only working for 3D)
+        objectRenderer.material.color = objectColor; //set new color to sphere (currently only working for 3D
+                                                     //ISSUE 2D sprite turns black...
+
+        //if (spriteRenderer) spriteRenderer.color = objectColor; 
     }
 
     // Start is called before the first frame update
